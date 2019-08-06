94398addc1b943c7be54b436f26356db18c2a26f
codechenx <codechenx@gmail.com>
2019-08-06 12:50:18 +0800

    modify some keywords

--- ISSUtils.py
+++ ISSUtils.py
@@ -1,21 +1,6 @@
 import os
-
-
-class Criterion:
-    """
-    The DataClass to store ISS criterion
-    Returns:
-        criterion {dict} -- ISS criterion
-        {"头颈部":{'1':["头部外伤","头痛"],'2':["嗜睡"]}}
-    """
-    def __init__(self):
-        self.criterion = None
-    
-    def get_critersions(self):
-        return self.criterion
-    
-    def set_critersions(self, criterion):
-        self.criterion = criterion
+from Criterion import Criterion
+import re
 
 class Calculator:
     
@@ -25,11 +10,13 @@ class Calculator:
 
     def cal_AIS(self, diagnose_description):
         AIS = {}
-        for body_part, scores in self.criterion_obj.get_critersions().items():
+        
+        for body_part, regions in self.criterion_obj.get_critersions().items():
             max_score = 0
-            for score, keywords in scores.items():
-                for keyword in keywords:
-                    if keyword in diagnose_description and int(score) > max_score:
+            for _, region in regions.items():
+                for score, keywords in region.items():
+                    match = [re.search(keyword, diagnose_description) for keyword in keywords]
+                    if any(match) and int(score) > max_score:
                         max_score = int(score)
             AIS[body_part]  = max_score
         return AIS
@@ -38,11 +25,6 @@ class Calculator:
         assert isinstance(AIS, dict) and len(AIS) != 0
         sorted_values  = sorted(AIS.values())
         top_3_values = sorted_values[-3:]
-        ISS = sum([i**2 for i in top_3_values])
+        
+        ISS =  75 if max(top_3_values) == 6 else sum([i**2 for i in top_3_values])
         return ISS
-    
-
-
-
-
-
--- test_ISSUtils.py
+++ test_ISSUtils.py
@@ -1,22 +1,26 @@
 from unittest import TestCase
 from  ISSUtils import *
-
+from Criterion import Criterion
 
 class TestUtils(TestCase):
-    def test_Calculator(self):
-        test_critersions = {
-            "头颈部":{'1':["头部外伤","头痛"],'2':["嗜睡"]},
-            "面部": {'1':["擦伤"],'2':["角膜裂伤"]}
-        }
+    def test_critersion(self):
+        import re 
+        criterion_obj = Criterion()
+        for body_part, regions in criterion_obj.get_critersions().items():
+            for _, region in regions.items():
+                for score, keywords in region.items():
+                    for keyword in keywords:
+                        re.search(keyword, "test")
+    
+    def test_calculator(self):
         criterion = Criterion()
-        criterion.set_critersions(test_critersions)
         cal = Calculator(criterion)
-        desp = "头痛安工大撒广东省"
+        desp = "头痛安工大撒广东省12根肋骨骨折"
         AIS = cal.cal_AIS(desp)
         ISS = cal.AIS_to_ISS(AIS)
-        assert ISS == 1
+        assert ISS == 9
 
-        desp = "头痛安工大撒广东省,角膜裂伤阿发嘎嘎理工"
+        desp = "头痛安工大撒广东省,角膜裂伤阿发嘎嘎理工,肾上腺挫伤重度"
         AIS = cal.cal_AIS(desp)
         ISS = cal.AIS_to_ISS(AIS)
-        assert ISS == 5
+        assert ISS == 4
